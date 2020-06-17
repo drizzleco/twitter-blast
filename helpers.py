6d225a9470d6a83e321fce7756540baae25e2ae9
petabite <jmslca123@gmail.com>
2020-06-17 16:48:26 -0700

    docs and improvements

--- helpers.py
+++ helpers.py
@@ -1,37 +1,69 @@
+from typing import List
+import tweepy, time
 
-def divide_into_chunks(l, n):
+
+def divide_into_chunks(l: List, n: int):
+    """
+    Split a list into chunks of n size
+
+    params:
+        l(list) - list to split up
+        n(int) - size of sublists
+    """
     for i in range(0, len(l), n):
         yield l[i : i + n]
+
+
 def print_progress_bar(
-    iteration,
-    total,
-    prefix="",
-    suffix="",
-    decimals=1,
-    length=50,
-    fill="█",
-    print_end="\r",
+    iteration: int,
+    total: int,
+    prefix: str = "",
+    suffix: str = "",
+    decimals: int = 1,
+    length: int = 50,
+    fill: str = "█",
+    print_end: str = "\r",
 ):
     """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        print_end    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    Call in a loop to create terminal progress bar. Print new line on complete
+
+    params:
+        iteration(int) - current iteration 
+        total(int) - total iterations 
+        prefix(str) - prefix string 
+        suffix(str) - suffix string 
+        decimals(int) - positive number of decimals in percent complete 
+        length(int) - character length of bar 
+        fill(str) - bar fill character 
+        print_end(str) - end character (e.g. "\r", "\r\n") 
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     bar = fill * filled_length + "-" * (length - filled_length)
     print("\r%s |%s| %s%% %s" % (prefix, bar, percent, suffix), end=print_end)
-    # Print New Line on Complete
     if iteration == total:
         print()
 
+
+def rate_limit_handler(cursor: tweepy.Cursor):
+    """
+    Handler for tweepy Cursors and automatically stops
+    execution when rate limit is reached
+
+    params:
+        cursor(tweepy.Cursor) - cursor to handle
+    """
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            print("Oh no!! We hit the rate limit. Resuming in 15 mins.")
+            time.sleep(15 * 60)
+
+
 def bye():
+    """
+    Stop execution of script.
+    """
     print("Ok bye.")
     exit()
--- tweet_blast.py
+++ tweet_blast.py
@@ -1,7 +1,8 @@
 from secrets import CONSUMER_KEY, CONSUMER_SECRET
-import tweepy, time, argparse, os, pickle
+import tweepy, time, argparse, os, pickle, pydoc
+from typing import List, Tuple
 from models import User, db
-from helpers import bye, print_progress_bar, divide_into_chunks
+from helpers import bye, print_progress_bar, divide_into_chunks, rate_limit_handler
 from app import app
 
 db.app = app
@@ -9,6 +10,14 @@ db.init_app(app)
 
 
 def auth():
+    """
+    Reads in user keys if previously saved. If not saved, take user
+    through Sign in with Twitter and serialize keys to a file.
+    returns fully setup OAuthHandler 
+
+    returns:
+        tweepy.OAuthHandler - OAuthHandler to plug into tweepy.API call
+    """
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     if os.path.isfile("keys"):
         token = pickle.load(open("keys", "rb"))
@@ -18,32 +27,21 @@ def auth():
             redirect_url = auth.get_authorization_url()
         except tweepy.TweepError:
             print("Error! Failed to get request token.")
-
-        print("visit to authorize with twitter: ", redirect_url)
-        verifier = input("paste the verification code here: ")
+        print("Visit to authorize with twitter: ", redirect_url)
+        verifier = input("Paste the verification code here: ")
         try:
             token = auth.get_access_token(verifier)
         except tweepy.TweepError:
             print("Error! Failed to get access token.")
-        # save token in keys file
         pickle.dump(token, open("keys", "wb"))
     return auth
 
 
-api = tweepy.API(auth())
-print("Logged in as: " + api.me().screen_name)
-
-
-def rate_limit_handler(cursor):
-    while True:
-        try:
-            yield cursor.next()
-        except tweepy.RateLimitError:
-            print("Oh no!! We hit the rate limit. Resuming in 15 mins.")
-            time.sleep(15 * 60)
-
-
 def fetch_followers():
+    """
+    Use tweepy to fetch user's followers' ids and then fetch their user objects
+    and save to the db.
+    """
     user_keys = [
         "id_str",
         "name",
@@ -81,13 +79,90 @@ def fetch_followers():
     print("Done!")
 
 
-def send_message(user_id, message):
-    api.send_direct_message(user_id, message)
+def send_message(user_id: str, message: str):
+    """
+    Send message to user_id
+
+    params:
+        user_id(str) - user id of user as string
+        message(str) - message to send
+    """
+    try:
+        api.send_direct_message(user_id, message)
+    except tweepy.RateLimitError:
+        print("Oh no!! We hit the rate limit. Resuming tomorrow.")
+        time.sleep(24 * 60 * 60)
 
 
-def mass_dm_followers(message, dry_run=True):
-    followers = db.session.query(User.id_str, User.screen_name).all()
+def ranked_followers(rank_by: str, value: str) -> List[Tuple[str, str]]:
+    """
+    Rank followers based on specified criteria using db queries. Return a list
+    of ranked followers' user id and their screen names who have not already
+    been sent a DM.
+
+    params:
+        rank_by(str) - criteria to rank by
+        value(str) - value to search for. only used for location 
+                     and description filter
+    """
+    if rank_by == "recent":
+        ranking = {"column": None}
+    elif rank_by == "followers_count":
+        ranking = {"column": User.followers_count}
+    elif rank_by == "following_count":
+        ranking = {"column": User.friends_count}
+    elif rank_by == "listed_count":
+        ranking = {"column": User.listed_count}
+    elif rank_by == "favourites_count":
+        ranking = {"column": User.favourites_count}
+    elif rank_by == "statuses_count":
+        ranking = {"column": User.statuses_count}
+    elif rank_by == "location":
+        ranking = {"location": value}
+    elif rank_by == "description":
+        ranking = {"description": value}
+    else:
+        raise Exception("No such ranking as " + rank_by)
+    if "column" in ranking.keys():
+        return (
+            db.session.query(User.id_str, User.screen_name)
+            .order_by(db.desc(**ranking))
+            .filter_by(dm_sent=False)
+            .all()
+        )
+    else:
+        for key, value in ranking.items():
+            return (
+                db.session.query(User.id_str, User.screen_name)
+                .filter(
+                    getattr(User, key).like("%{}%".format(value)), User.dm_sent == False
+                )
+                .all()
+            )
+
+
+def mass_dm_followers(
+    message: str, rank_by: str = "recent", value: str = "", dry_run: bool = True
+):
+    """
+    Send mass DM to all followers in order of specificed ranking and set
+    dm_sent flag for that user to True. Allows for dry run where messages
+    are not actually sent out and dm_sent flag is not changed.
+
+    params:
+        message(str) - message to send out
+        rank_by(str) - ranking method
+        value(str) - value to search for. only used for location 
+                     and description filter
+        dry_run(bool) - set to True to only pretend to send messages
+    """
+    followers = ranked_followers(rank_by, value)
     total_followers = len(followers)
+    if not total_followers:
+        print(
+            "No followers matched your criteria or they may have all been sent DMs already :("
+        )
+        bye()
     print()
     if dry_run:
         print("Dry run is ON. Messages are not actually being sent. Phew")
@@ -97,11 +172,25 @@ def mass_dm_followers(message, dry_run=True):
         print_progress_bar(i + 1, total_followers, suffix="Sent")
         if dry_run:
             time.sleep(0.01)
-        # else:
-        #     send_message(id, message) # DONT UNCOMMENT UNLESS U REALLY KNOW WHAT UR DOIN
+        else:
+            db.session.query(User).filter_by(id_str=id).update({"dm_sent": True})
+            db.session.commit()
+            # send_message(id, message) # DONT UNCOMMENT UNLESS U REALLY KNOW WHAT UR DOIN
+
+
+def reset_dm_sent_flag():
+    """
+    Reset all followers dm_sent column to False
+    """
+    db.session.query(User).update({"dm_sent": False})
+    db.session.commit()
+    print("Followers DM sent flags reset!")
 
 
 def logout():
+    """
+    Delete keys file
+    """
     if os.path.isfile("keys"):
         os.remove("keys")
         print("Logged out!")
@@ -110,36 +199,78 @@ def logout():
 
 
 if __name__ == "__main__":
+    api = tweepy.API(auth())
+    print("Logged in as: " + api.me().screen_name)
+    # parser config
     parser = argparse.ArgumentParser(
         description="Mass DM tool for Twitter to convert followers to another platform",
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
         "action",
-        choices=["send", "fetch", "logout"],
-        help="What do you wanna do?\n  - fetch - fetch followers\n  - send - send mass DM to followers\n  - logout - delete stored keys",
+        choices=["send", "fetch", "preview", "reset", "logout"],
+        help="""
+        What do you wanna do?
+        - fetch - fetch followers
+        - send - send mass DM to followers
+        - preview - preview followers list ranking
+        - reset - reset DM sent flags
+        - logout - delete stored keys""",
     )
-    parser.add_argument("-m", "--message", help="Message to mass DM")
     parser.add_argument(
         "-dry", action="store_true", help="Don't actually send DMs. Just pretend."
     )
     args = parser.parse_args()
+
+    # set ranking and value if action is preview or send
+    if args.action == "preview" or args.action == "send":
+        ranking_choices = [
+            "recent",
+            "followers_count",
+            "following_count",
+            "statuses_count",
+            "listed_count",
+            "favourites_count",
+            "location",
+            "description",
+        ]
+        value = ""
+        print("Choose how you'd like to rank your followers:")
+        for num, choice in enumerate(ranking_choices):
+            print("{}) {}".format(str(num + 1), choice))
+        ranking = ranking_choices[int(input("Enter the number of your choice: ")) - 1]
+        if ranking == "location" or ranking == "description":
+            value = input("Enter what you want to look for in {}: ".format(ranking))
+
+    # execute actions
     if args.action == "fetch":
+        # fetch action
         if os.path.isfile("followers.sqlite"):
             refetch = input(
                 "You've already fetched your followers. Are you sure you want to refetch them? This could take a while. [y/n]: "
             )
-            if refetch == "y":
+            if refetch is "y":
                 fetch_followers()
             else:
                 bye()
         else:
             fetch_followers()
-    elif args.action == "send":
-        if args.message:
-            message = args.message
+    elif args.action == "preview":
+        # preview action
+        followers = "Order of followers to be DM'ed(ranked by {} {}). Followers whom a DM hasn't been sent are shown:\n".format(
+            ranking, value
+        )
+        ranked_followers = ranked_followers(rank_by=ranking, value=value)
+        if ranked_followers:
+            for _, user in ranked_followers:
+                followers += user + "\n"
         else:
-            message = input("What do you wanna say? Type your message below:\n")
+            followers += "No followers matched this criteria :("
+        pydoc.pager(followers)
+    elif args.action == "send":
+        # send action
+        print("\nNOTE: you may want to preview your followers rankings before sending")
+        message = input("What do you wanna say? Type your message below:\n")
         confirmed = input(
             "Here is your message one more time:\n\n{}\n\nAre you sure you want to send this? [y/n]: ".format(
                 message
@@ -148,14 +279,17 @@ if __name__ == "__main__":
         if confirmed != "y":
             bye()
         if args.dry:
-            mass_dm_followers(message, dry_run=True)
+            mass_dm_followers(message, rank_by=ranking, value=value, dry_run=True)
         else:
             send = input(
                 "Dry run is not set. Are you sure you want to initiate the mass DM?? [y/n]: "
             )
             if send == "y":
-                mass_dm_followers(message, dry_run=True)
+                mass_dm_followers(message, rank_by=ranking, value=value, dry_run=False)
             else:
                 bye()
+    elif args.action == "reset":
+        reset_dm_sent_flag()
     elif args.action == "logout":
+        # logout action
         logout()
